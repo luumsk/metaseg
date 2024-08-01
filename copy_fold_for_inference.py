7038de624b6d8f7adefdc531a0f27f924bdb1384
khue <khue.luu@g.nsu.ru>
2024-08-01 18:38:48 +0700

    update copy_fold_for_inference.py with my dir

--- copy_fold_for_inference.py
+++ copy_fold_for_inference.py
@@ -5,10 +5,10 @@ import shutil
 
 FOLD_IDX = 4
 
-with open('/media/storage/suvorov/nnUNet_preprocessed/Dataset001_META/splits_final.json') as fp:
+with open('/media/storage/luu/nnUNet_preprocessed/Dataset111_Meta/splits_final.json') as fp:
     samples = json.load(fp)[FOLD_IDX]['val']
 
 for sample in samples:
-    sample_channels = glob.glob(f'/media/storage/suvorov/nnUNet_raw/Dataset001_META/imagesTr/{sample}_*.nii.gz')
+    sample_channels = glob.glob(f'/media/storage/luu/nnUNet_raw/Dataset111_Meta/imagesTr/{sample}_*.nii.gz')
     for ch in sample_channels:
-        shutil.copyfile(ch, f'/home/suvorov/meta_nnunet/inference_input/{os.path.basename(ch)}')
+        shutil.copyfile(ch, f'/home/luu/metaseg/inference_input/{os.path.basename(ch)}')
