ec8e2a638141928fef8e3060fcf150a6d353d1f9
khue <khue.luu@g.nsu.ru>
2024-08-01 18:34:41 +0700

    update copy to nnunet scripts

--- scripts/copy_meta24train.sh
+++ scripts/copy_meta24train.sh
@@ -1,18 +1,18 @@
-DATA_DIR="/media/storage/hdd_bair/BraTS24/MICCAI-BraTS2024-MET-Challenge-TrainingData"
+DATA_DIR="/media/storage1/BraTS24/MICCAI-BraTS2024-MET-Challenge-TrainingData"
 
-NNUNET_IMGS_DIR="/media/storage/suvorov/nnUNet_raw/Dataset003_META24/imagesTr"
-NNUNET_LBLS_DIR="/media/storage/suvorov/nnUNet_raw/Dataset003_META24/labelsTr"
+NNUNET_IMGS_DIR="/media/storage1/luu/nnUNet_raw/Dataset111_Meta/imagesTr"
+NNUNET_LBLS_DIR="/media/storage1/luu/nnUNet_raw/Dataset111_Meta/labelsTr"
 mkdir -p $NNUNET_IMGS_DIR
 mkdir -p $NNUNET_LBLS_DIR
 
-for SAMPLE in ${DATA_DIR}/*
+for SAMPLE_PTH in ${DATA_DIR}/*
 do
-    SAMPLE_IDX=$(basename "$SAMPLE" | cut -d '-' -f 3)
-    echo "Copying training sample #${SAMPLE_IDX}"
+    SAMPLE_ID=$(basename "$SAMPLE_PTH")
+    echo "Copying training sample ${SAMPLE_ID}"
 
-    cp "${SAMPLE}/BraTS-MET-${SAMPLE_IDX}-000-t1c.nii.gz" "${NNUNET_IMGS_DIR}/META24_${SAMPLE_IDX}_0000.nii.gz"
-    cp "${SAMPLE}/BraTS-MET-${SAMPLE_IDX}-000-t1n.nii.gz" "${NNUNET_IMGS_DIR}/META24_${SAMPLE_IDX}_0001.nii.gz"
-    cp "${SAMPLE}/BraTS-MET-${SAMPLE_IDX}-000-t2f.nii.gz" "${NNUNET_IMGS_DIR}/META24_${SAMPLE_IDX}_0002.nii.gz"
-    cp "${SAMPLE}/BraTS-MET-${SAMPLE_IDX}-000-t2w.nii.gz" "${NNUNET_IMGS_DIR}/META24_${SAMPLE_IDX}_0003.nii.gz"
-    cp "${SAMPLE}/BraTS-MET-${SAMPLE_IDX}-000-seg.nii.gz" "${NNUNET_LBLS_DIR}/META24_${SAMPLE_IDX}.nii.gz"
+    cp "${SAMPLE_PTH}/${SAMPLE_ID}-t1c.nii.gz" "${NNUNET_IMGS_DIR}/${SAMPLE_ID}_0000.nii.gz"
+    cp "${SAMPLE_PTH}/${SAMPLE_ID}-t1n.nii.gz" "${NNUNET_IMGS_DIR}/${SAMPLE_ID}_0001.nii.gz"
+    cp "${SAMPLE_PTH}/${SAMPLE_ID}-t2f.nii.gz" "${NNUNET_IMGS_DIR}/${SAMPLE_ID}_0002.nii.gz"
+    cp "${SAMPLE_PTH}/${SAMPLE_ID}-t2w.nii.gz" "${NNUNET_IMGS_DIR}/${SAMPLE_ID}_0003.nii.gz"
+    cp "${SAMPLE_PTH}/${SAMPLE_ID}-seg.nii.gz" "${NNUNET_LBLS_DIR}/${SAMPLE_ID}.nii.gz"
 done
