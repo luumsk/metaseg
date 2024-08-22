5502943df624df18661d16d4f3901d66e1ffcf52
khue <luumsk@gmail.com>
2024-08-23 04:27:45 +0700

    update split_folds.py to test_split_folds.py

---
+++ test_split_folds.py
@@ -0,0 +1,18 @@
+import os
+import json
+import pickle
+import numpy as np
+from sklearn.model_selection import KFold
+
+
+splits = None
+with open('splits_final.json', 'r') as f:
+    splits = json.load(f)
+
+# Test for duplicated patients in train and val sets
+print('Test duplicated patients in train and val sets')
+for i, fold in enumerate(splits):
+    train_patients = set(fn[:-4] for fn in fold['train'])
+    val_patients = set(fn[:-4] for fn in fold['val'])
+    intersection = train_patients & val_patients
+    print(f"Fold {i}: {intersection or 'clean'}")
