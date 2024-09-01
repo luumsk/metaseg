8bbb8db5cfc54b031d0aa3c31846f71a204064b9
khue <luumsk@gmail.com>
2024-09-01 15:09:08 +0700

    metrics

--- scripts/setvars.sh
+++ scripts/setvars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-export nnUNet_raw="/media/storage1/luu/nnUNet_raw"
-export nnUNet_preprocessed="/media/storage1/luu/nnUNet_preprocessed"
-export nnUNet_results="/media/storage1/luu/nnUNet_results"
+export nnUNet_raw="/media/storage/luu/nnUNet_raw"
+export nnUNet_preprocessed="/media/storage/luu/nnUNet_preprocessed"
+export nnUNet_results="/media/storage/luu/nnUNet_results"
